f6188f4cd7f5c8049161437e386b2af6e88b95ac
AnaMigriauli <anamigriauli1994@gmail.com>
2023-12-19 19:30:46 +0400

    Install prop-types for component prop validation

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,4 +1,10 @@
+import PropTypes from "prop-types";
 const Button = ({ children, className }) => {
   return <button className={className}>{children}</button>;
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 export default Button;
